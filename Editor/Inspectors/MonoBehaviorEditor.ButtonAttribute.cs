9ad7c7a9b3de91d3366e1c7d40ed9d31bb8d401e
Mario Debono <mario.debono@gmail.com>
2023-06-12 21:37:23 +0200

    fix: remove unused using statements
    SCM namespace was causing errors if missing in unity.

--- Editor/Inspectors/MonoBehaviorEditor.ButtonAttribute.cs
+++ Editor/Inspectors/MonoBehaviorEditor.ButtonAttribute.cs
@@ -1,10 +1,7 @@
-using Codice.CM.SEIDInfo;
 using System;
-using System.Collections.Generic;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 namespace MarioDebono.Inspector
 {
