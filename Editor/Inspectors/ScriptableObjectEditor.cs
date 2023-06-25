2defa7fea8f35e695df7526dccd53ff686d12742
Mario Debono <mario.debono@gmail.com>
2023-06-25 21:46:31 +0200

    feat: add button support to ScriptableObjects

---
+++ Editor/Inspectors/ScriptableObjectEditor.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace MarioDebono.Inspector
+{
+    [CustomEditor(typeof(ScriptableObject), true)]
+    [CanEditMultipleObjects]
+    public partial class ScriptableObjectEditor : Editor
+    {
+        Dictionary<MethodInfo, object[]> methodParameters = new Dictionary<MethodInfo, object[]>();
+        private bool buttonOpen;
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            foreach (var target in targets)
+            {
+                IEnumerable<MethodInfo> methods = target.GetType()
+                    .GetMethods(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+
+                ClearButtonHeader();
+                foreach (MethodInfo method in methods)
+                {
+                    ProcessButtonAttribute(method);
+                }
+            }
+        }
+    }
+}
